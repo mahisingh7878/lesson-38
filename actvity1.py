4d4dfe7124baac17c0b0a434588ae3e26affa255
mahisingh4415 <mahisinghadityasingh1232@gmail.com>
2024-10-26 21:27:56 +0530

    actvity1

---
+++ actvity1.py
@@ -0,0 +1,22 @@
+num=10
+
+name="Mahi"
+
+temp=43.5
+
+ishot=True
+
+print("datatype of num is ",type(num))
+print("datatype of name is ",type(name))
+print("datatype of temp is ",type(temp))
+print("datatype of ishot is ",type(ishot))
+
+#Typecasting the variables 
+num1=float(num)
+temp1=str(temp)
+ishot1=str(ishot)
+
+#After typecasting
+print("datatype of num is ",type(num1))
+print("datatype of temp is ",type(temp1))
+print("datatype of ishot is ",type(ishot1))
