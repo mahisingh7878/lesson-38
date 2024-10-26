332628bc4e64003b72c470374e5a5f00ff41c34b
mahisingh4415 <mahisinghadityasingh1232@gmail.com>
2024-10-26 21:54:04 +0530

    actvity2.

---
+++ actvity2.py
@@ -0,0 +1,21 @@
+num1=int(input("enter first number"))
+num2=int(input("enter second number"))
+
+sum=num1+num2
+diff=num1-num2
+quotient=num1/num2
+remainder=num1%num2
+div1=num1//num2
+prod=num1*num2
+
+print("sum of",num1,"and",num2,"is",sum)
+print("difference of",num1,"and",num2,"is",diff)
+print("product of",num1,"and",num2,"is",prod)
+print("quotient of",num1,"and",num2,"is",quotient)
+print("reminder of",num1,"and",num2,"is",remainder)
+print("floor division  of",num1,"and",num2,"is",div1)
+
+
+
+
+
